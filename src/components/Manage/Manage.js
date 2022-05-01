aa887edc9d1bf554be3b48a2cf67eef111a516b3
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-05-01 16:04:09 +0600

    code updated

--- src/components/Manage/Manage.js
+++ src/components/Manage/Manage.js
@@ -1,69 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
-import { Table } from 'react-bootstrap';
-import { toast } from 'react-toastify';
 import useItems from '../../hooks/useItems';
 import './Manage.css';
 import Loading from '../Loading/loading';
+import ItemTable from '../ItemTable/ItemTable';
 const Manage = () => {
     const [items, setItems] = useItems();
     if (!items) {
         return <Loading></Loading>
     }
-    const handleItemDelete = id => {
-        const proceed = window.confirm('Are you sure?');
-        if (proceed) {
-            console.log('deleted Id:', id);
-            const url = `http://localhost:5000/item/${id}`;
-            fetch(url, {
-                method: 'DELETE'
-            })
-                .then(res => res.json())
-                .then(data => {
-                    if (data.deletedCount > 0) {
-                        toast('Item Deleted.')
-                        const remaining = items.filter(item => item._id !== id);
-                        setItems(remaining);
-                    }
-                })
-        }
-    }
     return (
         <div className='shadow-lg p-3 mb-5 bg-body rounded'>
             <h2 className='fw-bolder text-center my-5'><span className='text-secondary'>MANAGE</span> ITEMS</h2>
-
-            <div className='items-section container'>
-                <Table striped bordered hover variant="dark" className=''>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Supplier Name</th>
-                            <th>Price</th>
-                            <th>Description</th>
-                            <th>Email</th>
-                            <th>Quantity</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            items.map(item => <tr>
-                                <td>{item._id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.supplierName}</td>
-                                <td>$ {item.price}</td>
-                                <td title={item.description}>{item.description.length > 100 ? item.description.slice(0, 101) + '...' : item.description}</td>
-                                <td>{item.email}</td>
-                                <td>{item.quantity}</td>
-                                <td className='text-center'><button onClick={() => handleItemDelete(item._id)} className='text-white bg-dark'><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></button></td>
-                            </tr>)
-                        }
-                    </tbody>
-                </Table>
-            </div>
-
+            <ItemTable items={items} setItems={setItems}></ItemTable>
         </div>
     );
 };
